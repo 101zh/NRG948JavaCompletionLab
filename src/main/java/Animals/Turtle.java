6a35d5b85c988f9e09b9bc960e445b375d6a2abd
101zh <67253838+101zh@users.noreply.github.com>
2024-11-21 15:52:55 -0800

    create example code

--- src/main/java/Animals/Turtle.java
+++ src/main/java/Animals/Turtle.java
@@ -4,5 +4,88 @@ package Animals;
  * A class that represents a simplified turtle.
  */
 public class Turtle {
-    // Class implementation here
+
+    public final static String turtleDescription = "Turtles are reptiles of the order Testudines, characterized by a special shell developed mainly from their ribs. Modern turtles are divided into two major groups, the Pleurodira (side necked turtles) and Cryptodira (hidden necked turtles), which differ in the way the head retracts. There are 360 living and recently extinct species of turtles, including land-dwelling tortoises and freshwater terrapins. They are found on most continents, some islands and, in the case of sea turtles, much of the ocean. Like other amniotes (reptiles, birds, and mammals) they breathe air and do not lay eggs underwater, although many species live in or around water.";
+    private int age;
+    private boolean angry;
+    private String name;
+    private String favoriteFood;
+
+    private double lazedHours = 0.0;
+
+    public Turtle(int age, boolean angry, String name, String favoriteFood) {
+        setAge(age);
+        setAngry(angry);
+        setName(name);
+        setFavoriteFood(favoriteFood);
+    }
+
+    public void eat(int quantity, String food) {
+        if (quantity > 3) {
+            System.out.println(name + " is shocked (⊙o⊙). It doesn't eat; there is too much food");
+            return;
+        } else if (!food.equals(favoriteFood)) {
+            System.out.println(name + " only wants to eat its favorite food");
+            return;
+        } else if (lazedHours < 3.0) {
+            System.out.println("(-^-)");
+            System.out.println(name + " just recently ate and can't eat anymore food");
+            return;
+        }
+
+        lazedHours = 0.0;
+        System.out.println(name + " happily eats the " + food + "! :)");
+    }
+
+    public void laze(double hours) {
+        if (hours >= 6.0) {
+            System.out.println(name + " shakes its head. It knows that " + hours + " hours is too much");
+            return;
+        } else if (lazedHours >= 6.0) {
+            System.out.println(name + " shakes its head. It has already lazed " + lazedHours+" hours");
+            return;
+        }
+
+        lazedHours += hours;
+        System.out.println(
+                name + " lazes for " + hours + " hours. "+name+" has now lazed consecutively for " + lazedHours + " hours");
+    }
+
+    public void setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            System.out.println("Invalid age; setting it to default");
+            age = 0;
+        }
+    }
+
+    public void setAngry(boolean angry) {
+        this.angry = angry;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFavoriteFood(String favoriteFood) {
+        this.favoriteFood = favoriteFood;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean getAngry() {
+        return angry;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavoriteFood() {
+        return favoriteFood;
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,75 @@
-// Don't FORGET YOUR IMPORTS!!!
+// Don't FORGET YOUR IMPORTS!!! (for both turtle and scanner)
 // example import: ``import foldername.classname``
 
-
+import java.util.Scanner;
+import Animals.Turtle;
 
 /**
  * The class where main software loop is ran.
  */
 public final class Main {
 
+    static Turtle turtle;
+
     /**
      * The firt method run for this application.
+     * 
      * @param args The command line arguments.
      **/
     public static void main(String[] args) {
-        // User interaction code goes here
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        System.out.println(Turtle.turtleDescription);
+        System.out.println("\n\n\nWelcome to turtle interactor!!");
+
+        System.out.println("It's time to create your turtle!");
+        createTurtle(userInput, scanner);
+
+        while (!userInput.equals("exit")) {
+            System.out.println("\n\nWhat would you like to do?");
+            System.out.println("1: View Your Turtle's Characteristics\n" +
+                    "2: Create a new Turtle\n" +
+                    "3: Feed Your Turtle\n" +
+                    "4: Let Your Turtle Laze\n");
+
+            userInput = scanner.nextLine();
+            System.out.println();
+            if (userInput.equals("1")) {
+                System.out.println("Name: " + turtle.getName() +
+                        "\nAge: " + turtle.getAge() +
+                        "\nAngry: " + turtle.getAngry() +
+                        "\nFavorite Food: " + turtle.getFavoriteFood());
+            } else if (userInput.equals("2")) {
+                createTurtle(userInput, scanner);
+            } else if (userInput.equals("3")) {
+                System.out.println("What food?");
+                String food = scanner.nextLine();
+                System.out.println("How much?");
+                int quantity = scanner.nextInt();
+                scanner.nextLine();
+                turtle.eat(quantity, food);
+            } else if (userInput.equals("4")) {
+                System.out.println("How long (in hours)?");
+                Double duration = scanner.nextDouble();
+                scanner.nextLine();
+                turtle.laze(duration);
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void createTurtle(String userInput, Scanner scanner) {
+        System.out.println("What is its name?");
+        String nameTurtle = scanner.nextLine();
+        System.out.println("What is its age? (# w/o decimal)");
+        int ageTurtle = scanner.nextInt();
+        System.out.println("Is it angry? (true/false)");
+        boolean angryTurtle = scanner.nextBoolean();
+        scanner.nextLine();
+        System.out.println("What is its favorite food?");
+        String foodTurtle = scanner.nextLine();
+
+        turtle = new Turtle(ageTurtle, angryTurtle, nameTurtle, foodTurtle);
     }
 }
