c88bf60dc5fca2a16342881535328d54022e4193
101zh <67253838+101zh@users.noreply.github.com>
2024-11-27 18:19:16 -0800

    add java docs and comments

--- src/main/java/Animals/Turtle.java
+++ src/main/java/Animals/Turtle.java
@@ -14,6 +14,14 @@ public class Turtle {
     // Stores the number of consecutive lazed hours
     private double consecutiveLazedHours = 0.0;
 
+    /**
+     * Creates an new instance of the turtle object
+     * 
+     * @param age          the age of the turtle in years
+     * @param isAngry      a boolean representing whether or not the turtle is angry
+     * @param name         the name of the turtle
+     * @param favoriteFood the name of the turtle's favorite food
+     */
     public Turtle(int age, boolean isAngry, String name, String favoriteFood) {
         setAge(age);
         setIsAngry(isAngry);
@@ -21,39 +29,62 @@ public class Turtle {
         setFavoriteFood(favoriteFood);
     }
 
+    /**
+     * Attempts to feed the turtle {@code quantity} times with the food:
+     * {@code food}
+     * 
+     * @param quantity the quantity of food fed to the turtle {@code quantity > 0}
+     * @param food     the type of food fed to the turtle
+     */
     public void eat(int quantity, String food) {
+        // Checking if requirements are met to eat
         if (quantity > 3) {
             System.out.println(name + " is shocked (⊙o⊙). It doesn't eat; there is too much food");
             return;
         } else if (!food.equals(favoriteFood)) {
             System.out.println(name + " only wants to eat its favorite food");
             return;
-        } else if (lazedHours < 3.0) {
+        } else if (consecutiveLazedHours < 3.0) {
             System.out.println("(-^-)");
             System.out.println(name + " just recently ate and can't eat anymore food");
             return;
         }
 
-        lazedHours = 0.0;
+        // If the eat method isn't ended early because a requirement is violated the eat
+        // method runs
+        consecutiveLazedHours = 0.0;
         for (int i = 0; i < quantity; i++)
             System.out.println(name + " happily eats a " + food + "! :)");
     }
 
+    /**
+     * Attempts to have the turtle laze {@code hours} hours
+     * 
+     * @param hours the amount of hours that the turtle will laze {@code hours > 0}
+     */
     public void laze(double hours) {
+        // Checking if requirements are met to laze
         if (hours > 6.0) {
             System.out.println(name + " shakes its head. It knows that " + hours + " hours is too much");
             return;
-        } else if (lazedHours >= 6.0) {
-            System.out.println(name + " shakes its head. It has already lazed " + lazedHours + " hours");
+        } else if (consecutiveLazedHours >= 6.0) {
+            System.out.println(name + " shakes its head. It has already lazed " + consecutiveLazedHours + " hours");
             return;
         }
 
-        lazedHours += hours;
+        // If the laze method isn't ended early because a requirement is violated the
+        // laze method runs
+        consecutiveLazedHours += hours;
         System.out.println(
-                name + " lazes for " + hours + " hours. " + name + " has now lazed consecutively for " + lazedHours
+                name + " lazes for " + hours + " hours. " + name + " has now lazed consecutively for " + consecutiveLazedHours
                         + " hours");
     }
 
+    /**
+     * Sets the age of the turtle
+     * 
+     * @param age the age of the turtle in years {@code age > 0}
+     */
     public void setAge(int age) {
         if (age >= 0) {
             this.age = age;
@@ -63,30 +94,65 @@ public class Turtle {
         }
     }
 
+    /**
+     * Sets if the turtle is angry
+     * 
+     * @param isAngry a boolean representing whether or not the turtle is angry
+     */
     public void setIsAngry(boolean isAngry) {
         this.isAngry = isAngry;
     }
 
+    /**
+     * Sets the name of the turtle
+     * 
+     * @param name the name of the turtle
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Sets the turtle's favorite food
+     * 
+     * @param favoriteFood the name of the turtle's favorite food
+     */
     public void setFavoriteFood(String favoriteFood) {
         this.favoriteFood = favoriteFood;
     }
 
+    /**
+     * Gets the age of the turtle in years
+     * 
+     * @return the age of the turtle in years
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Gets if the turtle is angry or not
+     * 
+     * @return a boolean representing whether or not the turtle is angry
+     */
     public boolean getIsAngry() {
         return isAngry;
     }
 
+    /**
+     * Gets the turtle's name
+     * 
+     * @return the name of the turtle
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the name of the turtle's favorite food
+     * 
+     * @return the name of the turtle's favorite food
+     */
     public String getFavoriteFood() {
         return favoriteFood;
     }
